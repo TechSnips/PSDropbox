75956b1b7a9ba5f073e4a88fc7dfcf225e045de8
2awn <anthony@howell-it.com>
2019-02-01 19:04:53 -0800

    Scaffold

---
+++ build.ps1
@@ -0,0 +1,22 @@
+#Requires -Modules psake
+[cmdletbinding()]
+param(
+    [string[]]$Task = 'manual'
+)
+
+$DependentModules = @('Pester','Psake','PlatyPS')
+Foreach ($Module in $DependentModules){
+    If (-not (Get-Module $module -ListAvailable)){
+        Install-Module -name $Module -Scope CurrentUser
+    }
+    Import-Module $module -ErrorAction Stop
+}
+$env:ModuleTempDir = "$PSScriptRoot\build" #$env:TEMP
+$env:ModuleName = "PSDropbox"
+$env:Author = "Anthony Howell"
+$env:ModuleVersion = "0.0.1"
+[hashtable]$global:PrivateData = @{
+    ProjectUri = 'https://github.com/ThePoShWolf/PSDropbox'
+}
+# Builds the module by invoking psake on the build.psake.ps1 script.
+Invoke-PSake $PSScriptRoot\psake.ps1 -taskList $Task
