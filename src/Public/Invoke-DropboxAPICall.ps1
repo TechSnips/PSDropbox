f13246c149cdf9b1d1bc8289ac6a18c465ecd653
2awn <anthony@howell-it.com>
2019-02-04 20:21:21 -0800

    Added support for an output file path

--- src/Public/Invoke-DropboxAPICall.ps1
+++ src/Public/Invoke-DropboxAPICall.ps1
@@ -8,7 +8,8 @@ Function Invoke-DropboxAPICall {
         [hashtable]$Header,
         [string]$subDomain = 'api',
         [ValidateSet('application/json','text/plain','application/octet-stream','application/octet-stream; charset=utf-8')]
-        [string]$ContentType = 'application/json'
+        [string]$ContentType = 'application/json',
+        [string]$FilePath
     )
     $BaseURI = "https://$subDomain.dropboxapi.com/2"
 
@@ -18,12 +19,14 @@ Function Invoke-DropboxAPICall {
         'Content-Type' = $ContentType
     }
 
-    ForEach($h in $header.GetEnumerator()){
-        $baseHeaders[$h.key] = $h.value
+    If($Header){
+        ForEach($h in $header.GetEnumerator()){
+            $baseHeaders[$h.key] = $h.value
+        }
     }
 
     If($subDomain -eq 'content'){
-        Invoke-WebRequest -Uri "$BaseURI/$Resource" -Method $Method -Headers $baseheaders -Body $body -OutFile C:\tmp\image.jpg
+        Invoke-WebRequest -Uri "$BaseURI/$Resource" -Method $Method -Headers $baseheaders -Body $body -OutFile $FilePath
     }Else{
         Invoke-RestMethod -Uri "$BaseURI/$Resource" -Method $Method -Headers $baseheaders -Body $body -Verbose
     }
