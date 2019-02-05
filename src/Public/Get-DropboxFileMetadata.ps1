01748cc61d25c2f58bc10d4db73db465521daabd
2awn <anthony@howell-it.com>
2019-02-04 20:21:05 -0800

    Gets file info

---
+++ src/Public/Get-DropboxFileMetadata.ps1
@@ -0,0 +1,22 @@
+Function Get-DropboxFileMetadata {
+    [cmdletbinding()]
+    Param(
+        [Parameter(
+            ParameterSetName = 'ByPath'
+        )]
+        [string]$Path,
+        [Parameter(
+            ParameterSetName = 'ById'
+        )]
+        [string]$Id
+    )
+    $Resource = 'files/get_metadata'
+    Switch($PSCmdlet.ParameterSetName){
+        'ByPath' {$data = $path}
+        'ById' {$data = $Id}
+    }
+    $Body = @{
+        path = $data
+    } | ConvertTo-Json
+    Invoke-DropboxAPICall -Method Post -Resource $Resource -Body $body
+}
