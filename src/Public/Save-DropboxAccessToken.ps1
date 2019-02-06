491b47856eb3aff9b9c0774939968420e2e64a90
Anthony Howell <anthony@howell-it.com>
2019-02-06 10:55:58 -0800

    Now can turn the auth code into a user token

--- src/Public/Save-DropboxAccessToken.ps1
+++ src/Public/Save-DropboxAccessToken.ps1
@@ -1,8 +1,27 @@
 Function Save-DropboxUserToken {
     Param(
-        [string]$AcessToken = $AuthConfig.AccessToken
+        [ValidateNotNullOrEmpty()]
+        [string]$AuthorizationCode = $AuthConfig.AuthCode,
+        [string]$AppKey,
+        [string]$AppSecret,
+        [string]$RedirectURI
     )
-    If($AccessToken){
+    $baseuri = 'https://api.dropboxapi.com/oauth2/token'
 
+    $encodedRedirect = [System.Web.HttpUtility]::UrlEncode($RedirectURI)
+
+    $encodedAuth = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$AppKey`:$AppSecret"))
+
+    $headers = @{
+        'Content-Type' = 'application/x-www-form-urlencoded'
+        Authorization = "Basic $encodedAuth"
     }
+
+    $body = @(
+        "code=$AuthorizationCode"
+        "grant_type=authorization_code"
+        "redirect_uri=$encodedRedirect"
+    ) -join '&'
+
+    $resp = Invoke-RestMethod -Uri $baseuri -Method Post -Headers $headers -Body $body
 }
