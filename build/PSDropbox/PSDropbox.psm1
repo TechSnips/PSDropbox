a83e350da59bc39a2a498ea4c927ef0a0f214f23
Anthony Howell <anthony@howell-it.com>
2019-02-06 10:59:15 -0800

    updated build script, built latest

--- build/PSDropbox/PSDropbox.psm1
+++ build/PSDropbox/PSDropbox.psm1
@@ -189,20 +189,82 @@ Function Request-DropboxAccessToken {
         Will get an auth code
     #>
 }
+Add-Type -AssemblyName System.Web
+Add-Type -AssemblyName System.Windows.Forms
 Function Request-DropboxAuthorizationCode {
     Param (
         [string]$AppKey,
-        [string]$AppSecret
+        [string]$AppSecret,
+        [string]$RedirectURI
     )
-    <#
-        Will get an authorization code
-    #>
+    $encodedRedirect = [System.Web.HttpUtility]::UrlEncode($RedirectURI)
+    $encodedKey = [System.Web.HttpUtility]::UrlEncode($AppKey)
+    $baseUri = 'https://www.dropbox.com/oauth2/authorize'
+    $parameters = "client_id=$encodedKey&redirect_uri=$encodedRedirect&response_type=code"
+    $uri = "$baseUri`?$parameters"
+
+    # Build a form to use with our web object
+    $Form = New-Object -TypeName 'System.Windows.Forms.Form' -Property @{
+        Width = 680
+        Height = 640
+    }
+
+    # Build the web object to brows to the logon uri
+    $Web = New-Object -TypeName 'System.Windows.Forms.WebBrowser' -Property @{
+        Width = 680
+        Height = 640
+        Url = $uri
+    }
+
+    # Add the document completed script to detect when the code is in the uri
+    $DocumentCompleted_Script = {
+        if ($web.Url.AbsoluteUri -match "error=[^&]*|code=[^&]*") {
+            $form.Close()
+        }
+    }
+
+    # Add controls to the form
+    $web.ScriptErrorsSuppressed = $true
+    $web.Add_DocumentCompleted($DocumentCompleted_Script)
+    $form.Controls.Add($web)
+    $form.Add_Shown({ $form.Activate() })
+
+    # Run the form
+    [void]$form.ShowDialog()
+
+    # Parse the output
+    $QueryOutput = [System.Web.HttpUtility]::ParseQueryString($web.Url.Query)
+    $Response = @{ }
+    foreach ($key in $queryOutput.Keys) {
+        $Response["$key"] = $QueryOutput[$key]
+    }
+
+    $Response
 }
 Function Save-DropboxUserToken {
     Param(
-        [string]$AcessToken = $AuthConfig.AccessToken
+        [ValidateNotNullOrEmpty()]
+        [string]$AuthorizationCode = $AuthConfig.AuthCode,
+        [string]$AppKey,
+        [string]$AppSecret,
+        [string]$RedirectURI
     )
-    If($AccessToken){
+    $baseuri = 'https://api.dropboxapi.com/oauth2/token'
 
+    $encodedRedirect = [System.Web.HttpUtility]::UrlEncode($RedirectURI)
+
+    $encodedAuth = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$AppKey`:$AppSecret"))
+
+    $headers = @{
+        'Content-Type' = 'application/x-www-form-urlencoded'
+        Authorization = "Basic $encodedAuth"
     }
+
+    $body = @(
+        "code=$AuthorizationCode"
+        "grant_type=authorization_code"
+        "redirect_uri=$encodedRedirect"
+    ) -join '&'
+
+    $resp = Invoke-RestMethod -Uri $baseuri -Method Post -Headers $headers -Body $body
 }
